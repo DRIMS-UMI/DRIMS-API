051544194ae90f6b15b1435f5c61789f67bb08b5
joshbaz <jkimbareeba@yahoo.com>
2025-07-05 13:17:09 +0300

    Added email routes

--- src/api/v1/index.mjs
+++ src/api/v1/index.mjs
@@ -1,13 +1,15 @@
 import express from "express";
 import managementRoutes from './routes/managementRoutes.js'
 import facultyRoutes from './routes/facultyRoutes.js'
+import emailRoutes from './routes/emailRoutes.js'
 import { requestLogger } from "./middleware/requestLogger.mjs";
 
 const router = express.Router();
 
 const endpoints = [
     {path: '/management', router: managementRoutes},
-    {path: '/faculty', router: facultyRoutes}
+    {path: '/faculty', router: facultyRoutes},
+    {path: '/email', router: emailRoutes}
 ];
 
 endpoints.forEach((endpoint)=> router.use(endpoint.path, requestLogger, endpoint.router));
