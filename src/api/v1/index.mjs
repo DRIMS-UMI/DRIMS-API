8f2c3055af7d0cf3653978d328b3c59e5ea63943
joshbaz <jkimbareeba@yahoo.com>
2025-07-10 00:42:07 +0300

    Added Supervisor url

--- src/api/v1/index.mjs
+++ src/api/v1/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import managementRoutes from './routes/managementRoutes.js'
 import facultyRoutes from './routes/facultyRoutes.js'
+import supervisorRoutes from './routes/supervisorRoutes.js'
 import emailRoutes from './routes/emailRoutes.js'
 import { requestLogger } from "./middleware/requestLogger.mjs";
 
@@ -9,6 +10,7 @@ const router = express.Router();
 const endpoints = [
     {path: '/management', router: managementRoutes},
     {path: '/faculty', router: facultyRoutes},
+    {path: '/supervisor', router: supervisorRoutes},
     {path: '/email', router: emailRoutes}
 ];
 
